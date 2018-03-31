1c3b8004ea70a023108890b7019946ad32284334
Jholdy <jholdydamasceno@gmail.com>
2018-03-30 23:44:00 -0300

    Create proveder apiServiceProvader and create model User and create method getUsers for api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  users: [];
+  constructor(public navCtrl: NavController, public apiService: ApiProvider) {}
 
-  constructor(public navCtrl: NavController) {
-
+  ionViewDidLoad(){
+    this.apiService.allUser().then(data => {
+      this.users = data.data;
+    });
   }
 
 }
