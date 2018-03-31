016986fbb5f8aed4c2e6698ea8041438945186a8
marcialwushu <marcialwushu@gmail.com>
2018-03-30 21:00:52 -0300

    pacote npm angularfire2 e firebase adicionado e estrutura de conexão criada no app.module e firebase.credentials.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { FIREBASE_CONFIG } from './firebase.credentials';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -21,7 +24,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDNoLztCehx8evMmIECxjepabQno-dUt8s",
+    authDomain: "demofirebase-1c239.firebaseapp.com",
+    databaseURL: "https://demofirebase-1c239.firebaseio.com",
+    projectId: "demofirebase-1c239",
+    storageBucket: "demofirebase-1c239.appspot.com",
+    messagingSenderId: "1097819452311"
+
+};
